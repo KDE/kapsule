dc0d55f1db3a27ff66afb81524f1b5d8bb448bda
Lasath Fernando <devel@lasath.org>
2026-01-27 23:37:29 -0600

    add sysext.sh script for building, serving and pulling kapsule

---
+++ sysext.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -xe
+
+kde-builder --no-src --build-when-unchanged kapsule
+
+serve_dir=/tmp/kapsule/
+mkdir -p "$serve_dir"
+
+tar -cf ${serve_dir}/kapsule.tar -C ~/src/kde/sysext/kapsule .
+
+python -m http.server --directory "$serve_dir" 8000 &
+trap "kill $!" EXIT
+
+ssh root@192.168.100.157 importctl pull-tar --class=sysext --verify=no --force http://192.168.100.1:8000/kapsule.tar
